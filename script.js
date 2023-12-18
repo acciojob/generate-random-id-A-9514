e1b2506eceaae8e9496ed70b76e59d8eacab8eb5
Aishwarya <142329750+A-9514@users.noreply.github.com>
2023-12-18 20:22:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-function makeid(l) {
-  // write your code here
+function generate(){
+    var length = document.getElementById("los").value;
+    var result = random(length);
+    document.getElementById("result").value = result;
 }
-
-// Do not change the code below.
-const l = prompt("Enter a number.");
-alert(makeid(l));
+function random(length){
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~";
+    for (var i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+function calc(){
+    var calc1 = document.getElementById("in-1").value;
+    var calc2 = document.getElementById("in-2").value;
+    var result = calc1 * calc2;
+    document.getElementById("los").value = result;
+    generate();
+}
